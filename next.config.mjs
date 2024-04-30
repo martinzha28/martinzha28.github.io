2b39c991782ec8343fd8a16940273b33e81756ee
martinzha28 <martinzha28@gmail.com>
2024-04-30 00:26:13 -0400

    yolo v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: '',
-    basePath: '',
+    images: {
+        unoptimized: true,
+      },
 };
 
 export default nextConfig;
